32861160be3c9a04ccc11d654f369b429be1d7a2
Kathryn Panger <kpanger88@gmail.com>
2023-03-28 18:21:53 -0500

    meters to nearest facility function

--- entities/dataframes/tractsToFacilities.py
+++ entities/dataframes/tractsToFacilities.py
@@ -12,15 +12,16 @@ class TractsToFacilities():
         assert self.tracts.city.lower() == self.facilities.city.lower()
         self.city = self.tracts.city
         self.state = self.tracts.state
+        self.df = None
 
     def get_meters_to_nearest_facility(self):
         tracts_cartesian = self.tracts.df.to_crs("EPSG:2953")
         facilities_cartesian = self.facilities.df.to_crs("EPSG:2953")
-        tracts_to_facilities = tracts_cartesian.sjoin_nearest(facilities_cartesian, how="left",
+        df = tracts_cartesian.sjoin_nearest(facilities_cartesian, how="left",
                                                       distance_col="meters_to_nearest_facility")
         # If emissions in data, sort by emissions to get "nearest, then most pollutant plant"
-        if self.tracts.varDict["62. ON-SITE RELEASE TOTAL"]:
-            tracts_to_facilities = tracts_to_facilities.sort_values(by=[self.tracts.varDict["62. ON-SITE RELEASE TOTAL"]], ascending=False)
+        if self.facilities.varDict["62. ON-SITE RELEASE TOTAL"]:
+            df = df.sort_values(by=[self.facilities.varDict["62. ON-SITE RELEASE TOTAL"]], ascending=False)
         else:
             print('First nearest facility kept. To instead keep most pollutant nearest facility, include \"ON-SITE RELEASE TOTAL\" in tracts variable list')
 
@@ -28,11 +29,11 @@ class TractsToFacilities():
         df = df.drop_duplicates(subset="GEOID", keep="first")
         df = df[["GEOID", "meters_to_nearest_facility"]]
         if not self.df:
-            self.df = tracts_to_facilities
+            self.df = df
         else:
-            self.df = self.tracts_to_facilities.join(tracts_to_facilities, on = "GEOID")
+            self.df = self.df.join(df, on = "GEOID")
 
-    def get_facilities_in_tract(self):
+    def get_facilities_count_in_tract(self):
         # facilities_in_tract = gpd.sjoin(self.facilities.df, self.tracts.df, how="left", op='intersects')
         # # Add a field with 1 as a constant value
         # facilities_in_tract['number_of_facilities_in_tract'] = 1
@@ -76,6 +77,7 @@ if __name__ == "__main__":
 
     # combine
     tractsToFacilities = TractsToFacilities(tracts, facilities)
+    tractsToFacilities.get_meters_to_nearest_facility()
 
     # print
     print(tractsToFacilities.df.head())
